f25d3870244fd9893469675e5c20c94a04feb745
eawapownikov <eawapownikov@gmail.com>
2018-11-10 09:07:13 +0300

    Add mochajs

---
+++ test/first.js
@@ -0,0 +1,32 @@
+const wdio = require("webdriverio");
+const scrollUntill = require("../utils/scroll-untill");
+
+const opts = {
+  port: 4723,
+  desiredCapabilities: {
+    platformName: "Android",
+    platformVersion: "8.1",
+    deviceName: "Android Emulator",
+    app: process.cwd() + "/app-release.apk",
+    automationName: "UiAutomator"
+  }
+};
+
+const client = wdio.remote(opts);
+
+describe("Платные статьи", () => {
+  it("test", async () => {
+    await client.init().waitForVisible("~Settings", 6000, false);
+    await scrollUntill(client, "~Article 21");
+
+    await client
+      .click("~Article 21")
+      .pause(2000)
+      .end()
+      .catch(e => {
+        console.log(e);
+      });
+
+    return true;
+  });
+});
